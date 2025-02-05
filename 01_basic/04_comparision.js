cf12b0916f836ba20f4cea96d8d7a61ce836bc3e
Piyush180 <78400227+Piyush180@users.noreply.github.com>
2025-02-05 20:40:49 +0000

    DATA  TYPE

--- 01_basic/04_comparision.js
+++ 01_basic/04_comparision.js
@@ -5,4 +5,21 @@ const id = Symbol('123')
 
 const accId = Symbol('123')
 
-console.log(id === accId);
+//       console.log(id === accId);
+
+/**************************NON PREEMETIVE DATA TYPE*************************8 */
+
+const skills = ["js", " react", "c++"]; // array
+
+let myObj {                
+ name = " caydfdsj",
+ age = 23,
+}                                        // object
+
+
+const myFunction = Function(){          // function
+    console.log("hello world");
+    
+}
+
+
