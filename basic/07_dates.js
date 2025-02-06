acd090d79db95ffcb448c06a1e3eb39f424233a2
Piyush180 <78400227+Piyush180@users.noreply.github.com>
2025-02-06 12:48:04 +0000

    dates

---
+++ basic/07_dates.js
@@ -0,0 +1,20 @@
+let myDate =  new Date()
+
+//   console.log(myDate.toString());
+//   console.log(myDate.toISOString());
+//   console.log(myDate.toJSON());
+//   console.log(myDate.toLocaleDateString());
+ 
+// console.log(typeof myDate);                               // object
+
+
+let myTimeStamp = Date.now();
+
+
+// console.log(myTimeStamp);
+ console.log(myDate.getTime());
+// console.log(Math.floor(Date.now()/1000));  // seconds as /1000
+
+myDate.toLocaleDateString(`defauly`{
+    weekday = 'long'                                   // mydate se jo date milegi usko modify kar diya or abb weekday lon me milenge
+})
