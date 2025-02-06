2dfdbdd5f64de5fefd4adc78a32240bed7ec0d0e
Piyush180 <78400227+Piyush180@users.noreply.github.com>
2025-02-06 11:51:35 +0000

    nums and math

---
+++ basic/06_numsANDmath.js
@@ -0,0 +1,37 @@
+
+const score = 400;
+const balance = new Number(100);
+//console.log(balance);
+
+
+//console.log(balance.toString().length);  // balance ko string me convert kar diya
+
+//console.log(balance.toFixed(1));          // exactl  digits after the decimal poin
+
+const otherNumber = 145.236547
+//console.log(otherNumber.toPrecision(5));  // 0 to provided index rounded off value
+
+
+
+
+// +++++++++++++++++++++++++  MATH   ++++++++++++++++++++
+
+console.log(Math);
+
+//  console.log(Math.abs(-4));       // abc = absolute value , -ve ko +ve
+//  console.log(Math.round(4.3));   // round-off value
+//  console.log(Math.ceil(4.3));
+//  console.log(Math.floor(4.3));
+ 
+//  console.log(Math.random());            // 0-1 ke beech random value
+//  console.log((Math.random()*10) + 1 ); // value > 1 dega
+
+const Min = 10;
+const Max = 20;
+
+//  console.log(Math.floor(Math.random()* (Max - Min + 1)+ Min));  // 10-20 ke beech me value dega
+
+
+
+
+
