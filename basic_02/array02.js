27052c80da03cb439d0246db066d132dae671cf5
Piyush180 <78400227+Piyush180@users.noreply.github.com>
2025-02-07 16:20:27 +0000

    array

---
+++ basic_02/array02.js
@@ -0,0 +1,18 @@
+const marvel = [ 'iroman', 'thor', 'captain'];
+                       // console.log(marvel);
+
+const dc = ['superman', 'flash', 'batman' ];
+
+const all_heroes = marvel.concat(dc)
+
+ //marvel.push(dc)
+
+                      // console.log(marvel);
+                      // console.log(all_heroes);
+
+
+const all_new_heroes = [...marvel, ...dc]  // combine both the array
+
+                       // console.log(all_new_heroes);
+
+                       
