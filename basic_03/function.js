e5c0a81a947e9fcc2925845d260090bf746c00e3
Piyush180 <78400227+Piyush180@users.noreply.github.com>
2025-02-08 21:49:38 +0000

    FUNCTIONS

--- basic_03/function.js
+++ basic_03/function.js
@@ -16,7 +16,7 @@ AddTwoNumber                                  // reference
 
 
 function logInUserMessage(username){
-    if(!username){                                                              //if(username === undefined)
+    if(!username){                                                              //if(username === undefined){}
         console.log("please enter the username")
         return
         
@@ -25,4 +25,41 @@ function logInUserMessage(username){
 
 }
 
-console.log(logInUserMessage());
+//console.log(logInUserMessage());
+
+
+function calculateCartPrice (...num1){                          // ...num1 = rest and spread operator, combines all the value of array
+    return num1;
+}
+
+//console.log(calculateCartPrice(200,400,800))
+
+
+
+//+++++++++++++++++++++++++++++++++++++++      OBJECT PASS IN FUCNCTION        ++++++++++++++++++++++++
+
+
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
