b3cc157d14420b4ca364c090e1b8a92607bb6c76
Piyush180 <78400227+Piyush180@users.noreply.github.com>
2025-02-08 13:43:52 +0000

    arrays02

--- basic_02/array02.js
+++ basic_02/array02.js
@@ -14,5 +14,13 @@ const all_heroes = marvel.concat(dc)
 const all_new_heroes = [...marvel, ...dc]  // combine both the array
 
                        // console.log(all_new_heroes);
+const another_array = [ 1,2,3,[5,6],9,[5,6,[1,2]]]
 
-                       
+const real_another_array = another_array.flat(Infinity);         // flat: returns a new array with all contancatinated array seperatly
+                    //console.log(real_another_array);
+
+ let score1 = 100;
+ let score2 = 200;
+ let score3 = 500;
+ 
+ console.log(Array.of(score1, score2, score3))  // .of multiple value ko single array me combine kar deta hai
