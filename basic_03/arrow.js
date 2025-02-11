d64e6de4d000845dbd032c05666fd9ccbb807953
Piyush180 <78400227+Piyush180@users.noreply.github.com>
2025-02-11 15:49:55 +0000

    arrow

--- basic_03/arrow.js
+++ basic_03/arrow.js
@@ -16,4 +16,28 @@ const user = {
 // user.username = "sam"                     // current content change kar diya 
 // console.log(user.welcomeMessage())
 
-console.log(this);
+// console.log(this);                            //{} kyu ki koi current context nahi hai 
+
+// function chai(){                                   // functions me thsi kaam nahi karta
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+
+// const chai =  () => {                                // arrow function
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+
+// chai()
+
+
+// ++++++++++++++++++++++++++++++++  ARROW FUNCTION ++++++++++++++++
+
+const addNumbers (num1 , num2) => {
+
+    return num1 + num2 
+}                 
+addNumbers(5,6)
+console.log(addNumbers);
