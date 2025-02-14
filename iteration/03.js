151b090d072ac7d6e7f914325621506143c94b04
Piyush180 <78400227+Piyush180@users.noreply.github.com>
2025-02-14 09:11:00 +0000

    for of

---
+++ iteration/03.js
@@ -0,0 +1,32 @@
+// for of
+const greeting = "hello"
+for (const greet of greeting) {
+    // console.log(`each element is ${greet}`)
+    
+}
+
+// ++++++++++++++++++++++++++++    MAPs ++++++++++++++++++++++++
+
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+const myObject = {                                    // maps iteratable hai, for loop nahi 
+    game1: 'NFS',
+    game2: 'Spiderman'         
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
